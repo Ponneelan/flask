26e694bd2e4ddc2633e0baf939f4a072f029fc31
vetri <vetrivendanucet@gmail.com>
2022-07-23 23:52:47 +0530

    Try to return output as Json Format

--- flirstapp.py
+++ flirstapp.py
@@ -1,11 +1,11 @@
 from distutils.log import debug
-from flask import Flask
+from flask import Flask,jsonify
 
 
 app = Flask(__name__)
-@app.route('/')
-def home():
-    return "Welcome all"
+@app.route('/index/<int:age>')
+def home(age):
+    return jsonify({'age':age})
 
 if __name__== '__main__':
     app.run(debug=True)
